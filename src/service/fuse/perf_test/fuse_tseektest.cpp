f18bf814c2107e4f56da8e666c40092433d1558e
chuhan <chuhan.ouyang@gmail.com>
2024-04-21 14:19:00 -0400

    show that lseek is update contents

--- src/service/fuse/perf_test/fuse_tseektest.cpp
+++ src/service/fuse/perf_test/fuse_tseektest.cpp
@@ -46,19 +46,15 @@ int main (int argc, char* argv[]) {
             num_runs = std::atoi(argv[++i]);
         }
     }
-    if (num_runs < 4){
-        std::cerr << "Must have at least 4 Runs\n";
-        return -1;
-    }
     size_t byte_size = kb_size * 1024;
-    std::vector<uint64_t> timeStampRuns = readCSV();
-    uint64_t firstTime = timeStampRuns.at(0);
-    uint64_t halfway  = timeStampRuns.at(num_runs/2);
-    uint64_t lastTime = timeStampRuns.at(num_runs-1);
+    // std::vector<uint64_t> timeStampRuns = readCSV();
+    // uint64_t firstTime = timeStampRuns.at(0);
+    // uint64_t halfway  = timeStampRuns.at(num_runs/2);
+    // uint64_t lastTime = timeStampRuns.at(num_runs-1);
 
-    for (uint64_t timeStamp : timeStampRuns){
-        std::cout << "Time: " << timeStamp <<'\n';
-    }
+    // for (uint64_t timeStamp : timeStampRuns){
+    //     std::cout << "Time: " << timeStamp <<'\n';
+    // }
     const char* filePath = "test/latest/pool/read_test";
     int file = open(filePath, O_RDWR);
     if (file < 0) {
@@ -66,13 +62,27 @@ int main (int argc, char* argv[]) {
         return 1;
     }
     std::vector<char> buffer(byte_size);
-    int SEEK_TIME = 10;
-    off_t offset0 = lseek(file, 0, SEEK_TIME);
-    if (offset0 == (off_t)-1) {
-        printf("Error seeking the file.\n");
+    // Move the file pointer to the desired offset
+    off_t newPosition = lseek(file, 5, SEEK_SET);
+    if (newPosition < 0) {
+        perror("Error seeking file");
         close(file);
         return 1;
     }
+    ssize_t bytesRead = read(file, buffer.data(), byte_size-5);
+    if (bytesRead < 0) {
+        perror("Error reading from file");
+        close(file);
+        return 1;
+    }
+    close(file);
+//    int SEEK_TIME = 10;
+//    off_t offset0 = lseek(file, 0, SEEK_TIME);
+//    if (offset0 == (off_t)-1) {
+//        printf("Error seeking the file.\n");
+//        close(file);
+//        return 1;
+//    }
     // std::cout << "Offset Should be 0: " << offset0 <<'\n';
 
     // off_t offset2 = lseek(file, firstTime, SEEK_TIME);
